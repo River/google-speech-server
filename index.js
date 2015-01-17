767a9168f0f687f13356fb45ba22e876adc064b7
River Jiang <river@seas.2864.apng.wireless-pennnet.upenn.edu>
2015-01-17 16:51:13 -0500

    copy paste code

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+// var express = require('express');
+// var app = express();
+//
+// app.set('port', (process.env.PORT || 5000));
+// app.use(express.static(__dirname + '/public'));
+//
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'));
+// });
+
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
+var multer = require('multer');
 var express = require('express');
+
 var app = express();
+var done = false;
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(multer({
+  dest: './uploads/',
+  // rename: function (fieldname, filename) {
+  //   return filename+Date.now();
+  // },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...')
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+    done=true;
+  }
+}));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.post('/', function (req, res) {
+  if (done == true) {
+    var uploadFilePath = req.files.upload.path;
+
+    var options = {
+      url: 'https://www.google.com/speech-api/v2/recognize?output=json&lang=en-us&key=AIzaSyC1H9uEIPDDfj53aFRWF2sDHtwY5C3ML_w',
+      headers: {
+        'Content-Type': 'audio/l16; rate=16000'
+      }
+    }
+
+    fs.createReadStream(uploadFilePath).pipe(request.post(options, function (err, httpRes, body) {
+      if (err) {
+        return console.error('upload failed: ', err);
+      }
+
+      body = body.replace('{"result":[]}\n','');
+      res.end(body);
+    }));
+
+  }
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+/*Run the server.*/
+app.listen(8080,function(){
+  console.log("Working on port 8080");
 });
